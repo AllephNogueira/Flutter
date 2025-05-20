a6022f65eefa682afae7fac6c292307e80a8dff6
Alleph <allephn@hotmail.com.br>
2025-05-20 16:25:37 -0300

    Criando primeiro projeto em Dart

---
+++ 03 - Linguagem Dart/01 - Primeiro projeto/meuprimeiroprojeto/bin/meuprimeiroprojeto.dart
@@ -0,0 +1,5 @@
+import 'package:meuprimeiroprojeto/meuprimeiroprojeto.dart' as meuprimeiroprojeto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${meuprimeiroprojeto.calculate()}!');
+}
---
+++ 03 - Linguagem Dart/01 - Primeiro projeto/meuprimeiroprojeto/test/meuprimeiroprojeto_test.dart
@@ -0,0 +1,8 @@
+import 'package:meuprimeiroprojeto/meuprimeiroprojeto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
