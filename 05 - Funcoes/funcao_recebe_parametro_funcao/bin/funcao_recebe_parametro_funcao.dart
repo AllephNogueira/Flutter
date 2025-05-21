24ab7a480bf1fccc4b07e7c96a503a4fb8d1d8d1
Alleph <allephn@hotmail.com.br>
2025-05-20 22:37:28 -0300

    arrow function

---
+++ 05 - Funcoes/funcao_recebe_parametro_funcao/bin/funcao_recebe_parametro_funcao.dart
@@ -0,0 +1,5 @@
+import 'package:funcao_recebe_parametro_funcao/funcao_recebe_parametro_funcao.dart' as funcao_recebe_parametro_funcao;
+
+void main(List<String> arguments) {
+  print('Hello world: ${funcao_recebe_parametro_funcao.calculate()}!');
+}
---
+++ 05 - Funcoes/funcao_recebe_parametro_funcao/test/funcao_recebe_parametro_funcao_test.dart
@@ -0,0 +1,8 @@
+import 'package:funcao_recebe_parametro_funcao/funcao_recebe_parametro_funcao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
