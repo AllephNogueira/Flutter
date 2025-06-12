da82e47aa21db95b8537193879dc927e5dd2efe2
Alleph <allephn@hotmail.com.br>
2025-06-12 17:39:50 -0300

    Criando tela de login

---
+++ 02 - Flutter/02 - Navegacao e controle de Acesso a paginas/tela_login/lib/main.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Login'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+
+        body: SizedBox(
+          /* Dizendo que nosso container pode ocupar toda a largura | Assim colocamos ele no meio. */
+          width: double.infinity,
+
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              /* Imagina como se aqui fosse um item, mas esse item esta transparente, apenas "Ocupando aquele espaço" */
+              const SizedBox(height: 50),
+
+              Row(
+                children: [
+                  Expanded(child: Container()),
+                  Expanded(
+                    flex: 8,
+                    child: Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+
+              SizedBox(height: 20),
+
+              const Text(
+                "Já tem cadastro?",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              const Text(
+                "Faça seu login e make the change._",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        "Digite seu e-mail:",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      flex: 3,
+                      child: Text(
+                        "email",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        "Digite sua senha:",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      flex: 3,
+                      child: Text(
+                        "senha",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                color: const Color.fromARGB(255, 34, 4, 102),
+                height: 30,
+                alignment: Alignment.center,
+                child: Text("ENTRAR", style: TextStyle(color: Colors.white)),
+              ),
+
+              Expanded(child: Container()),
+              SizedBox(height: 10),
+
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: Text(
+                  "Esqueci minha senha",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: Text(
+                  "Criar conta",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 60),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
