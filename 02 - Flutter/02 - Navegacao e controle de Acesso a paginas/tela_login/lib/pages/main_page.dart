6b08dc8963d25c59a9ecb997806b81b7dcf6c078
Alleph <allephn@hotmail.com.br>
2025-06-16 11:18:05 -0300

    Conhecendo o Drawer

---
+++ 02 - Flutter/02 - Navegacao e controle de Acesso a paginas/tela_login/lib/pages/main_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Painel de Controle")),
+
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: InkWell(
+                      child: const Text("Dados cadastrados"),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+
+                const Divider(),
+                const SizedBox(height: 10),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: InkWell(child: const Text("Saldo"), onTap: () {}),
+                  ),
+                ),
+
+                const Divider(),
+                const SizedBox(height: 10),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: InkWell(child: const Text("Sair"), onTap: () {}),
+                  ),
+                ),
+
+                const Divider(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
